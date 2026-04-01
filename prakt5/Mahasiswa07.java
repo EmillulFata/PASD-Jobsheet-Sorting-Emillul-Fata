c44e2848e1ba42f857f4527deab1526d290081c6
FataRach <emillulfr@gmail.com>
2026-04-01 10:56:02 +0700

    Prakt 5.3

---
+++ prakt5/Mahasiswa07.java
@@ -0,0 +1,25 @@
+package prakt5;
+
+public class Mahasiswa07 {
+  String nim;
+  String nama;
+  String kelas;
+  double ipk;
+
+  Mahasiswa07() {
+  }
+
+  Mahasiswa07(String nm, String name, String kls, double ip) {
+    nim = nm;
+    nama = name;
+    kelas = kls;
+    ipk = ip;
+  }
+
+  void tampilInformasi() {
+    System.out.println("Nama  : " + nama);
+    System.out.println("NIM   : " + nim);
+    System.out.println("Kelas : " + kelas);
+    System.out.println("IPK   : " + ipk);
+  }
+}
